be8e4fd0551242bc430275028bc11f950c44c944
Peter Kiss <peter@yscik.com>
2017-09-18 02:16:02 +0200

    build changes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 
-import 'file-loader?name=[name].[ext]!./focus';
+import 'file-loader?name=[name].[ext]!./content';
 import Vue from 'vue'
 import Search from './Search.vue'
 import './icon.vue'
+import './focus'
 
 if(window.focused)
 new Vue({
