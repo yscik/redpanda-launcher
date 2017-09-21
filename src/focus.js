15abefa6ccbf9917c5ff0ca845fe7f81f3797efb
Peter Kiss <peter@yscik.com>
2017-09-22 00:53:43 +0200

    focus hack update

--- src/focus.js
+++ src/focus.js
@@ -1,16 +1,19 @@
 
-(function focus() {
+
+(async function focus () {
 
   const URL = location.href;
-  if(true||location.search === '?f') return window.focused = true;
+  if(location.search.startsWith('?f'))
+  {
+    browser.history.deleteUrl({ url : URL.replace(/\?.*$/, '') });
+    return window.focused = true;
+  }
 
-  return browser.tabs.getCurrent((tab) => {
+  browser.tabs.getCurrent((tab) => {
     const tabId = tab.id;
     browser.tabs.create({ url : URL+'?f'}, () => {
       browser.tabs.remove(tabId);
     });
   });
-
-  browser.history.deleteUrl({ url : URL });
 
 })();
