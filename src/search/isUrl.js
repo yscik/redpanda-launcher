6e7c6cb53f15637c5a10b3e5479cbf2f03e62d98
Peter Kiss <peter@yscik.com>
2017-11-06 02:54:10 +0100

    improved url recognition

--- src/search/isUrl.js
+++ src/search/isUrl.js
@@ -3,15 +3,24 @@ import tld_txt from 'raw-loader!../data/tlds-alpha-by-domain.txt'
 const tldInFile = /^[a-zA-Z]/;
 
 const protocol = /^(about:|.*:\/\/)/;
-const urlPattern = /.+\.(\w+)(?:[\/:?#]*)?/;
 const tlds = tld_txt.split('\n').filter(t => t && tldInFile.test(t)).map(t => t.toLowerCase());
 
 function isUrl(term)
 {
   if(!term || term.includes(' ')) return false;
   if(protocol.test(term)) return true;
-  let [,tld] = urlPattern.exec(term)||[];
-  return tld && tlds.includes(tld)
-};
+  try {
+    const {hostname, port} = new URL('http://'+term);
+    if(port) return true;
+    const tldDot = hostname.lastIndexOf('.');
+    if(tldDot < 0) return false;
+    let tld = hostname.substr(tldDot+1);
+    console.log(tld);
+    return tlds.includes(tld)
+  }
+  catch(err) {
+    return false;
+  }
+}
 
 export { isUrl, protocol}
