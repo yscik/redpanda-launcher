46c63949a09a826b5fe904d05a4fe076954dbb61
Peter Kiss <peter@yscik.com>
2017-11-20 21:58:06 +0100

    Revert "debounce input event"
    This reverts commit ad5b43b

--- src/search/input-complete.vue
+++ src/search/input-complete.vue
@@ -6,35 +6,15 @@
 
 import Vue from 'vue'
 
-const DEBOUNCE_DELAY = 100;
-const DEBOUNCE_LIMIT = 300;
-
 export default Vue.component('input-complete',
 {
   props: ['value', 'completeTo'],
   data: function() { return {displayvalue: ''}},
-  created() {
-    this.lastEmit = 0;
-    this.debouncing = null;
-  },
   methods: {
     update: function(value)
     {
-      this.debounce(() => this.$emit('input', value))
-    },
-    debounce(fn) {
-      let t = performance.now(),
-          delay = true;
-      if(t - this.lastEmit > DEBOUNCE_LIMIT) delay = false;
-
-      const callFn = () => {
-        fn();
-        this.lastEmit = t;
-      };
-
-      if (this.debouncing) clearTimeout(this.debouncing);
-      if(delay) this.debouncing = setTimeout(callFn, DEBOUNCE_DELAY);
-      else callFn();
+//      this.displayvalue = value;
+      this.$emit('input', value)
     },
     suggest: function(suggestion)
     {
